931df5af8a2a46a455d0b98c28f9f6e3606507ba
Ulyana Sichkar <uvrubel@gmail.com>
2021-02-06 19:12:02 +0300

    next routing, css styled components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children }) {
   return (
     <div>
+        <Header></Header>
       <h2>I am the page comp</h2>
       {children}
     </div>

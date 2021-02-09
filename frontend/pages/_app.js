c982fccf7bfb288d2b0f04ae28813618eb5aa190
Ulyana Sichkar <uvrubel@gmail.com>
2021-02-09 21:39:30 +0300

    nprogress for route changes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,11 @@
 import Page from '../components/Page';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import '../components/styles/nprogress.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps}) {
   return (

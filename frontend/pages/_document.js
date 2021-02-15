8a3141f659bf1458f4389109727459d07b682b88
Ulyana Sichkar <uvrubel@gmail.com>
2021-02-15 20:41:40 +0300

    get stylesheet in document

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,15 +1,25 @@
-import Document, { Html, Head, NextScript, Main } from 'next/document';
+import Document, {Html, Head, NextScript, Main} from 'next/document';
+import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head/>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    //load css before everything else
+    static getInitialProps({renderPage}) {
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(App => props => sheet.collectStyles(<App {...props}/>));
+        const styleTags = sheet.getStyleElement();
+
+        return { ...page, styleTags}
+    }
+
+    render() {
+        return (
+        <Html>
+            <Head/>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+        );
+    }
 }

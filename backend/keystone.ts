6c568718b8e02f09e5073a35e9b0184d9b006df0
Ulyana Sichkar <uvrubel@gmail.com>
2021-02-23 16:35:25 +0300

    setting up apollo client

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -5,6 +5,7 @@ import {User} from './schemas/User';
 import {Product} from './schemas/Product';
 import {ProductImage} from './schemas/ProductImage';
 import {withItemData, statelessSessions} from '@keystone-next/keystone/session'
+import {insertSeedData} from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -33,8 +34,12 @@ export default withAuth(
         },
         db: {
             adapter: 'mongoose',
-            url: databaseURL
-            //TODO add data seeding here
+            url: databaseURL,
+
+            async onConnect(keystone) {
+                if (process.argv.includes('--seed-data'))
+                    await insertSeedData(keystone);
+            }
         },
         lists: createSchema({
             //chema items go in here
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,15 +2,34 @@ import Page from '../components/Page';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import '../components/styles/nprogress.css'
+import {ApolloProvider} from '@apollo/client'
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps}) {
-  return (
-    <Page>
-      <Component {...pageProps}/>
-    </Page>
-  );
+function MyApp({ Component, pageProps, apollo}) {
+    console.log(apollo);
+    return (
+        <ApolloProvider client={apollo}>
+            <Page>
+                <Component {...pageProps}/>
+            </Page>
+        </ApolloProvider>
+    );
 }
+
+//if component uses getInitialProps method, component should wait data fetching
+
+MyApp.getInitialProps = async function({Component, ctx}) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+    }
+
+    pageProps.query = ctx.query;
+    return {pageProps};
+}
+
+export default withData(MyApp);
